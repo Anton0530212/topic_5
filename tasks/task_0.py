a24d5e171f678770beeae17acd5e8dd2e26b7400
Антон Плотицын <plotitsynanton@yandex.ru>
2023-09-11 22:49:06 +0300

    - Правильное: №0, №1, №2, №3, №4, №5, №6, №7, №8 - Неправильное: - Улучшены: - На доработку: №8

--- tasks/task_0.py
+++ tasks/task_0.py
@@ -1,5 +1,7 @@
-colors = ["Red", "Green", "White", "Black", "Pink", "Yellow"]
+# Нужно поменять местами первый и последний элементы этого списка
+colors: list = ["Red", "Green", "White", "Black", "Pink", "Yellow"]
 
-...  # Ваш код
+colors[0]: list = "Yellow"
+colors[-1]: list = "Red"
 
 print(colors)
