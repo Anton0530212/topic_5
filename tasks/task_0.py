2fb08f788351e94829b8283717dea65af7b6dc2f
Антон Плотицын <plotitsynanton@yandex.ru>
2023-09-14 12:54:10 +0300

    исправлена задача 0

--- tasks/task_0.py
+++ tasks/task_0.py
@@ -1,8 +1,6 @@
 # Нужно поменять местами первый и последний элементы этого списка
 colors: list = ["Red", "Green", "White", "Black", "Pink", "Yellow"]
 
-# Неправильно, места надо менять, а не писать вручную
-colors[0]: str = "Yellow"
-colors[-1]: str = "Red"
+colors[-1], colors[0] = colors[0], colors[-1]
 
 print(colors)
