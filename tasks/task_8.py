6b5562c6196d5d68c554d75b8ba0738488fb653c
Shoxrux Yuldashov <deagleshox@mail.ru>
2023-09-19 10:40:02 +0100

    Проверка ДЗ | 5. Основы коллекций в Python
    - Правильно:
    - Неправильно:
    - Улучшены: №8
    - На доработку:

--- tasks/task_8.py
+++ tasks/task_8.py
@@ -1,16 +1,34 @@
-fruits: dict = {'яблоко': 50, 'банан': 30, 'груша': 40, 'апельсин': 35}
+fruits: dict = dict(
+    [
+        ('яблоко', 50),
+        ('банан', 30),
+        ('груша', 40),
+        ('апельсин', 35)
+    ]
+)
 
-print("Список фруктов и их цены:")
-print('Цена апельсин -', fruits['апельсин'])
+print('Список фруктов и их цены:\n', fruits, sep='', end='\n\n')
 
 user_input: str = input('Выберите фрукт из списка: ')
+price: dict = fruits[user_input]
 
-price: dict = dict({'яблоко': 50, 'банан': 30, 'груша': 40, 'апельсин': 35})
-print('Цена груша -', price['груша'])
+print('Цена', user_input, '-', price)
 
+# --------------------------------examples
 
-# Создание словаря с данными:
-# С помощью фигурных скобок
-# С помощью функции dict()
+# SELECT price FROM fruits;
+# WHERE name == user_input;
 
-# В теории есть примеры, по ним можно решить эту задачку
+# data_1 = [
+#     ('apple', 10),
+#     ('banana', 20),
+#     ('orange', 30)
+# ]
+#
+# print(dict(data_1))
+#
+#
+# data_2 = dict(apple=10, banana=20, orange=30)
+# data_3 = dict(яблоки=10, бананы=20, апельсины=30)
+# print(data_2)
+# print(data_3)
